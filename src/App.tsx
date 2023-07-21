8727e49ea3b1e3e183aa8c64742196be4b64fe4a
william1993jda <william.jda@hotmail.com>
2023-07-21 16:29:21 -0300

    feat: adiconado react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { BrowserRouter } from "react-router-dom"
+import { AppRoutes } from "./routes"
+
 export const App = () => {
 
   return (
-    <>
-      <h1>Tela de início</h1>        
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
