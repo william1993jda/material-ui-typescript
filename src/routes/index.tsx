ab5177c894f2f0580a5017435f45f427ed08cf1c
william1993jda <william.jda@hotmail.com>
2023-07-21 17:05:47 -0300

    feat: Configuração de temas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,10 @@
-
 import { Routes, Route, Navigate } from 'react-router-dom'
+import { Button } from '@mui/material'
 
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path='/pagina-inicial' element={<p>Página inícial</p>} />
+            <Route path='/pagina-inicial' element={<Button variant='contained' color='primary'>Página inícial</Button>} />
 
             <Route path='*' element={<Navigate to='/pagina-inicial' />} />
         </Routes>
